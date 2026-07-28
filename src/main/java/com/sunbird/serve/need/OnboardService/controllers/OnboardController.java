ab6f43b789e7cac53128cafae4ce17e943f0e545
Sowmya-Raghuram <sowmya.raghuram@evidyaloka.org>
2026-07-28 17:07:54 +0530

    Onboard-Entity apis

--- src/main/java/com/sunbird/serve/need/OnboardService/controllers/OnboardController.java
+++ src/main/java/com/sunbird/serve/need/OnboardService/controllers/OnboardController.java
@@ -29,7 +29,7 @@ import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/onboard")
+@RequestMapping("/entity-onboard")
 public class OnboardController {
 
     private static final Logger logger = LoggerFactory.getLogger(OnboardController.class);
